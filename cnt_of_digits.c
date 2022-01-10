0098e007fb59e75e85bcafb4c562438f77b81783
akshayvishwakarma00008 <76159696+akshayvishwakarma00008@users.noreply.github.com>
2022-01-10 22:51:03 +0530

    Update cnt_of_digits.c

--- cnt_of_digits.c
+++ cnt_of_digits.c
@@ -2,15 +2,18 @@
 
 #include<stdio.h>
 
-int main(){
+int main()
+{
     int cnt=0;
     long int num;
 
     printf("Enter a number to count\n");
     scanf("%ld",&num);
 
-    if(num>0){
-        while(num){
+    if(num>0)
+    {
+        while(num)
+        {
             cnt++;
             num=num/10;
         }
